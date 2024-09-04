75f716ba2fdbbafa24c751615460057cb4d06dfb
FructDev <luisfructuoso86@gmail.com>
2024-09-03 20:13:54 -0400

    Add MongodbUri

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: `https://df-backend-u6yp.onrender.com/api`, // Cambia esto a la URL de tu API
-  baseURL: `http://localhost:5000/api`, // Cambia esto a la URL de tu API
+  baseURL: `https://backend-lost-devices.onrender.com/api`, // Cambia esto a la URL de tu API
+  // baseURL: `http://localhost:5000/api`, // Cambia esto a la URL de tu API
 });
 
 api.interceptors.request.use((config) => {
